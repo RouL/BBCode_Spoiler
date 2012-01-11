c5565a98185fb2f706cc5d238bb845df690d48b7
Markus Bartz <roul@codingcorner.info>
2012-01-11 19:29:40 +0100

    Started with version 1.2.0 with jQuery use
    incremented version number
    deactivated JavaScript
    deactivated CSS

--- files/js/Spoiler.class.js
+++ files/js/Spoiler.class.js
@@ -4,35 +4,35 @@
  * @package		com.woltlab.community.roul.bbcode.spoiler
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-var Spoiler = Class.create({
-	initialize: function() {
-		// nothing to do here.
-	},
-	
-	register: function(spoilerElement) {
-		spoilerElement.down('.quoteBody').setStyle({
-			display: 'none'
-		});
-		spoilerElement.className = 'quoteBox spoiler jsSpoiler';
-		spoilerElement.down('h3').observe('click', this.toggle);
-	},
-	
-	toggle: function(event) {
-		spoilerElement = Event.element(event).up('.spoiler');
-		contentElement = spoilerElement.down('.quoteBody');
-		if (contentElement.visible()) {
-			Effect.BlindUp(contentElement, { duration: 0.5 });
-		}
-		else {
-			Effect.BlindDown(contentElement, { duration: 0.5 });
-		}
-	}
-});
-
-var spoiler = new Spoiler();
-
-document.observe("dom:loaded", function() {
-	$$('.spoiler').each(function(spoilerElement) {
-		spoiler.register(spoilerElement);
-	});
-});
+//var Spoiler = Class.create({
+//	initialize: function() {
+//		// nothing to do here.
+//	},
+//	
+//	register: function(spoilerElement) {
+//		spoilerElement.down('.quoteBody').setStyle({
+//			display: 'none'
+//		});
+//		spoilerElement.className = 'quoteBox spoiler jsSpoiler';
+//		spoilerElement.down('h3').observe('click', this.toggle);
+//	},
+//	
+//	toggle: function(event) {
+//		spoilerElement = Event.element(event).up('.spoiler');
+//		contentElement = spoilerElement.down('.quoteBody');
+//		if (contentElement.visible()) {
+//			Effect.BlindUp(contentElement, { duration: 0.5 });
+//		}
+//		else {
+//			Effect.BlindDown(contentElement, { duration: 0.5 });
+//		}
+//	}
+//});
+//
+//var spoiler = new Spoiler();
+//
+//document.observe("dom:loaded", function() {
+//	$$('.spoiler').each(function(spoilerElement) {
+//		spoiler.register(spoilerElement);
+//	});
+//});
